cff624c974d5d4ae7a827b9da9b1a91e1214bb4a
Nidhin.B <103826656+nidhinnidhin@users.noreply.github.com>
2025-03-11 17:22:45 +0530

    data fetching process inprogress

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-// asdfas
+// asdfaswe
 const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
